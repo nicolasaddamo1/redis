aee5259fc91279b0a57bc99bea7235ef87d47511
Nico <nicolasaddamo1@gmail.com>
2025-05-29 13:21:29 -0300

    feat: added new dependencies

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import axios from 'axios'
 
 const app = express()
 
-app.get('/characters', (req, res) => {
+app.get('/characters', async (req, res) => {
+    const { data } = await axios.get('https://rickandmortyapi.com/api/character')
+    return res.json(data)
+})
 
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
 })
