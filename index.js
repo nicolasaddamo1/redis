bea395ca7a546a989059f26df25bc67aecaaca1f
Nico <nicolasaddamo1@gmail.com>
2025-05-29 13:25:01 -0300

    feat: changed port

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import axios from 'axios'
+import responseTime from 'response-time'
 
 const app = express()
+
+app.use(responseTime())
 
 app.get('/characters', async (req, res) => {
     const { data } = await axios.get('https://rickandmortyapi.com/api/character')
     return res.json(data)
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+app.listen(3001, () => {
+    console.log('Server is running on http://localhost:3001')
 })
