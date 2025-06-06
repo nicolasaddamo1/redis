ed1a927576915bb54e83b1ebb9713192f7288e9b
Nico <nicolasaddamo1@gmail.com>
2025-06-06 17:11:54 -0300

    feat: added new redis feature

--- index.js
+++ index.js
@@ -20,10 +20,29 @@ app.get('/characters', async (req, res) => {
     const { data } = await axios.get('https://rickandmortyapi.com/api/character')
 
     const saveResult = await client.set('characters', JSON.stringify(data))
+    console.log(saveResult)
+    if (saveResult !== 'OK') {
+        console.error('Failed to save data to Redis')
+        return res.status(500).json({ error: 'Internal Server Error' })
+    }
 
     return res.json(data)
 })
 
+app.get('/characters/:id', async (req, res) => {
+    const { id } = req.params
+
+    const reply = await client.get(id)
+
+    if (reply) return res.json(JSON.parse(reply))
+
+    const { data } = await axios.get(`http://rickandmortyapi.com/api/character/${id}`)
+
+    const saveResult = await client.set(id, JSON.stringify(data))
+    console.log(saveResult)
+
+    return res.json(data)
+})
 
 const main = async () => {
     await client.connect()
