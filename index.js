df4d14a0b42a53bea48b9b74ef8473c8e264e9e9
Nico <nicolasaddamo1@gmail.com>
2025-06-06 16:56:18 -0300

    feat: added redis, working

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 import express from 'express'
 import axios from 'axios'
 import responseTime from 'response-time'
+import { createClient } from 'redis'
 
 const app = express()
+const client = createClient({
+    host: 'localhost',
+    port: 6379,
+})
 
 app.use(responseTime())
 
 app.get('/characters', async (req, res) => {
+
+    const reply = await client.get('characters')
+
+    if (reply) return res.json(JSON.parse(reply))
+
     const { data } = await axios.get('https://rickandmortyapi.com/api/character')
+
+    const saveResult = await client.set('characters', JSON.stringify(data))
+
     return res.json(data)
 })
 
-app.listen(3001, () => {
-    console.log('Server is running on http://localhost:3001')
-})
+
+const main = async () => {
+    await client.connect()
+    app.listen(3001)
+    console.log('Connected to Redis, server is running on port 3001')
+}
+
+main()
